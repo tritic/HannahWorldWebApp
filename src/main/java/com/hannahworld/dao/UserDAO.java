e40fab3a8698210094a9d4e645f8f970ba6f3c7d
yongxu <xuyhanghang@gmail.com>
2015-06-28 22:04:13 -0600

    first commit

---
+++ src/main/java/com/hannahworld/dao/UserDAO.java
@@ -0,0 +1,9 @@
+package com.hannahworld.dao;
+
+import java.util.List;
+
+import com.hannahworld.model.User;
+
+public interface UserDAO {
+	public List<User> list();
+}
---
+++ src/main/java/snippet/Snippet.java
@@ -0,0 +1,12 @@
+package snippet;
+
+public class Snippet {
+	/*
+	use mysql;
+	update user set password='null' where User='root';
+	flush privileges;
+	quit;
+	*/
+	
+}
+
